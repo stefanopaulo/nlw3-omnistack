74c2857458e3661085407304d1dbe078837e1ff6
stefanopaulo <stefanopaulo47@gmail.com>
2020-10-13 11:05:51 -0300

    cadastrando um novo orfanato

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 
 import './database/connection';
 
@@ -6,8 +8,32 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-    return res.json({ "message": "Hello Wolrd" });
+app.post('/orphanages', async (req, res) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = req.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return res.json({ message: "Hello Wolrd" });
 });
 
 app.listen(3333);
