5cf5d3cbe79dcfb80cce6192099d3a7b19fe4302
stefanopaulo <stefanopaulo47@gmail.com>
2020-10-13 14:05:37 -0300

    lidando com exceções

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,6 +1,6 @@
 import { ErrorRequestHandler } from 'express';
 
-const errorHandler: ErrorRequestHandler = (error, req, res) => {
+const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
     console.log(error);
 
     return res.status(500).json({ message: 'Internal server error' });
