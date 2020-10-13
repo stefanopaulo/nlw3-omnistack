9209fba26d28087ef4b4d82bca17c52ae8917750
stefanopaulo <stefanopaulo47@gmail.com>
2020-10-13 14:19:01 -0300

    instalando o modulo cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,9 +4,11 @@ import 'express-async-errors';
 import './database/connection';
 import routes from './routes';
 import errorHandler from './errors/handler';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
